b3609fb91b6ef058caa6166ccb70fdf96f6614eb
sogwsc <sogwsc@126.com>
2017-07-21 16:32:25 +0800

    推送信息添加日期和幅度

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 ﻿# -*- coding: utf-8 -*-
-
 from url_info import *
 from spider import *
 import itchat
 from sleep import *
+from datetime import datetime
 
 sleep()
 @itchat.msg_register(itchat.content.TEXT)
@@ -13,6 +13,8 @@ def print_content(msg):
 itchat.auto_login(hotReload=True)
 itchat.run
 
+curTime = datetime.now().strftime('%y-%m-%d')
+itchat.send(curTime, 'filehelper')
 
 for (name, fund_id) in ids.items():
     url = 'http://fund.eastmoney.com/%s.html' % fund_id
@@ -21,10 +23,10 @@ for (name, fund_id) in ids.items():
     value_num = value[1:-1]
     name = name.decode('utf-8')
     if(sign == '-' and float(value_num)>2.5):
-        content = u'%s跌幅很大，可以补仓'
+        content = u'%s跌幅很大，可以补仓'+sign+value_num
         itchat.send(content % name, 'filehelper')
     else:
-        content = u'%s 不需要补仓'
+        content = u'%s 不需要补仓'+sign+value_num
         itchat.send(content % name, 'filehelper')
 
 
